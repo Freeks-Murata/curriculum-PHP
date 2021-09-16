744de342c007244a98258e6574868396f5a34e4e
Haya16-2450 <90101067+Freeks-Murata@users.noreply.github.com>
2021-09-16 19:27:35 +0900

    FizzBuzz問題 課題提出テスト
    index2_9.phpの作成

---
+++ 2-9/index2_9.php
@@ -0,0 +1,14 @@
+<?php
+for ($a = 1; $a <= 100; $a++) {
+    if ($a % 3 == 0 && $a % 5 == 0) {
+        echo "FizzBuzz!!" . "<br>";
+    } else if ($a % 3 == 0) {
+        echo "Fizz!" . "<br>";
+    } else if ($a % 5 == 0) {
+        echo "Buzz!" . "<br>";
+    } else {
+        echo $a . "<br>";
+    }
+}
+
+?>
