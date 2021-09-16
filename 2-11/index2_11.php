c49dd9f3a7bda21d1c3962bdff1f659f3671a054
Haya16-2450 <90101067+Freeks-Murata@users.noreply.github.com>
2021-09-17 00:56:11 +0900

    フォームに入力した内容を取得する 課題提出テスト
    index2_11.php
    result2_11.phpの作成

---
+++ 2-11/index2_11.php
@@ -0,0 +1,9 @@
+<form action="result2_11.php" method="POST">
+    名前：<input type="text" name="name">
+    <br>
+    メールアドレス：<input type="text" name="address">
+    <br>
+    パスワード：<input type="password" name="password">
+    <br>
+    <input type="submit" value="送信">
+</form>
---
+++ 2-11/result2_11.php
@@ -0,0 +1,7 @@
+<?php
+$name = $_POST["name"];
+$address = $_POST["address"];
+$password = $_POST["password"];
+echo "私の名前は、$name
+<br>メールアドレスは、$address
+<br>私のパスワードは、$password";
